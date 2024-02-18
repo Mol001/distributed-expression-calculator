633fafd0775161877ddfd059f1ca72970e77dbd5
Mol001 <94551791+Mol001@users.noreply.github.com>
2024-02-18 22:21:03 +0300

    Update main1.go

--- main1.go
+++ main1.go
@@ -29,17 +29,18 @@ var (
 
 var len_expr = make(map[string]int)
 
-var db = make(map[string]string)
+// var db = make(map[string]string)
 var agentConnected = make(map[string]string)
 
-var agent_1 int = 5
-var agent_2 int = 5
-var perts_expr = []string{}
-var db11 = make(map[int][]string)
+// var agent_1 int = 5
+// var agent_2 int = 5
+// var perts_expr = []string{}
+// var db11 = make(map[int][]string)
 var db22 = make(map[int][][]string)
 var recvest_id = make(map[string]int)
 var mutex sync.Mutex
 
+// обработка базовых операций
 func evaluateArithmeticExpression(expr string) int {
 	parts := []string{}
 	if strings.Contains(expr, "/") {
@@ -80,7 +81,7 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	mutex.Lock()
 	port := conn.LocalAddr().(*net.TCPAddr).Port
-	agentConnected[strconv.Itoa(port)] = "processing"
+	agentConnected[strconv.Itoa(port)] = "processing" // статус сервера говорящий о процессе вычисления
 	mutex.Unlock()
 	// Читаем выражение из соединения
 	reader := bufio.NewReader(conn)
@@ -88,8 +89,6 @@ func handleConnection(conn net.Conn) {
 
 	result := evaluateArithmeticExpression(strings.TrimSpace(expr))
 
-	// timer := time.NewTimer(time.Second * 2)
-	// <-timer.C
 	// Отправляем результат обратно оркестратору
 	_, err := conn.Write([]byte(strconv.Itoa(result) + "\n"))
 	if err != nil {
@@ -97,13 +96,14 @@ func handleConnection(conn net.Conn) {
 	}
 	mutex.Lock()
 	port1 := conn.LocalAddr().(*net.TCPAddr).Port
-	agentConnected[strconv.Itoa(port1)] = "finisd"
+	agentConnected[strconv.Itoa(port1)] = "finisd" // статус сервера говорящий о зевершенни последней операции
 	mutex.Unlock()
 }
 
+// Функция отправляющая разделенные на части выражения агентам,
+// организующая их последовательное выполнение и сожраняющая их
 func sendToAgent(expr_map map[int][][]string, ex_to_stat string, rec_id int) {
 	exprs := expr_map[rec_id]
-	fmt.Println(exprs)
 	for _, expr2 := range exprs {
 		go func(expr2 []string, rec_id int) {
 			expr := expr2[0] + expr2[1] + expr2[2]
@@ -281,10 +281,7 @@ func sendToAgent(expr_map map[int][][]string, ex_to_stat string, rec_id int) {
 							part = append(part, n_op)
 							part = append(part, result)
 							db22[rec_id] = append(db22[rec_id], part)
-							fmt.Println(len(db22[rec_id]), len_expr[ex_to_stat])
-
 							if len(db22[rec_id]) == len_expr[ex_to_stat] {
-								fmt.Println("=====================================================================================================")
 								err12 := UpdateStatus("status.txt", ex_to_stat, "true", strconv.Itoa(rec_id))
 								if err12 != nil {
 									log.Fatal(err12)
@@ -335,12 +332,8 @@ func sendToAgent(expr_map map[int][][]string, ex_to_stat string, rec_id int) {
 				part := []string{}
 				part = append(part, n_op)
 				part = append(part, result)
-
 				db22[rec_id] = append(db22[rec_id], part)
-				fmt.Println(len(db22[rec_id]), len_expr[ex_to_stat])
-
 				if len(db22[rec_id]) == len_expr[ex_to_stat] {
-					fmt.Println("=====================================================================================================")
 					err12 := UpdateStatus("status.txt", ex_to_stat, "true", strconv.Itoa(rec_id))
 					if err12 != nil {
 						log.Fatal(err12)
@@ -357,15 +350,14 @@ func sendToAgent(expr_map map[int][][]string, ex_to_stat string, rec_id int) {
 }
 
 func maxOperand(slice []string) (operand string) {
-	// slice := []string{"op2", "op1", "op22", "op7"}
 	var maxStr string
 	var maxNum int
 
 	for _, str := range slice {
 		match := regexp.MustCompile(`op(\d+)`).FindStringSubmatch(str)
 		if len(match) > 1 {
-			num, _ := strconv.Atoi(match[1]) // Convert the matched number string to an integer
-			if num > maxNum {                // Compare the number to the current max
+			num, _ := strconv.Atoi(match[1])
+			if num > maxNum {
 				maxNum = num
 				maxStr = str
 			}
@@ -389,15 +381,9 @@ func splitExpression(expr string) []string {
 	return matches
 }
 
-func containsOnlyArithmeticSignsAndNumbers(s string) bool {
-	// Regular expression to match only numbers and arithmetic signs
-	re := regexp.MustCompile(`^[0-9+\-*/]*$`)
-	return re.MatchString(s)
-}
-
+// проверка выражения на корректность
 func checkArithmeticExpression(s string) bool {
 	allowedChars := "0123456789+-*/"
-
 	numStarted := false
 	for i, char := range s {
 		if !strings.ContainsRune(allowedChars, char) {
@@ -416,32 +402,14 @@ func checkArithmeticExpression(s string) bool {
 			}
 			numStarted = false
 		}
-
 	}
 	return true
 }
 
-// func checkArithmeticExpression(s string) bool {
-// 	allowedChars := "0123456789+-*/"
-// 	prevChar := ' '
-// 	for _, char := range s {
-// 		if !strings.ContainsRune(allowedChars, char) {
-// 			return false
-// 		}
-// 		if char == '+' || char == '-' || char == '*' || char == '/' {
-// 			if prevChar == char {
-// 				return false
-// 			}
-// 			prevChar = char
-// 		}
-// 	}
-// 	return true
-// }
-
+// разбивка полученного выражения на части, которые будут отсылаться агентам
 func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 	operation_list := make(map[int][][]string, 0)
 	expr2 := splitExpression(expr)
-	fmt.Println(expr2)
 	midl_sl := [][]string{}
 	if len(expr2) == 3 {
 		sr := []string{}
@@ -453,9 +421,7 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 	} else {
 		k := 1
 		used_index := []string{}
-		// index := ""
 		index := []int{}
-		// sr2 := []string{}
 		for i, n := range expr2 {
 			if n == "*" || n == "/" {
 				if sliseContains(index, i-1) == false && sliseContains(index, i) == false && sliseContains(index, i+1) == false {
@@ -500,12 +466,8 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 					index = append(index, i)
 					index = append(index, i+1)
 
-					// sr1 := strings.Join(sr, "")
-					// sr2 = append(sr2, sr1)
 					midl_sl = append(midl_sl, sr)
-					// operation_list[strconv.Itoa(ri)] = midl_sl
 					k += 1
-					fmt.Println(operation_list, used_index, index)
 				} else if sliseContains(index, i-1) == false && sliseContains(index, i) == false && sliseContains(index, i+1) == true {
 					sr := []string{}
 					fp := ""
@@ -530,12 +492,8 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 					index = append(index, i)
 					index = append(index, i+1)
 
-					// sr1 := strings.Join(sr, "")
-					// sr2 = append(sr2, sr1)
 					midl_sl = append(midl_sl, sr)
-					// operation_list[strconv.Itoa(ri)] = midl_sl
 					k += 1
-					fmt.Println(operation_list, used_index, index)
 				} else if sliseContains(index, i-1) == true && sliseContains(index, i) == false && sliseContains(index, i+1) == true {
 					sr := []string{}
 					fp := ""
@@ -565,12 +523,8 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 					index = append(index, i)
 					index = append(index, i+1)
 
-					// sr1 := strings.Join(sr, "")
-					// sr2 = append(sr2, sr1)
 					midl_sl = append(midl_sl, sr)
-					// operation_list[strconv.Itoa(ri)] = midl_sl
 					k += 1
-					fmt.Println(operation_list, used_index, index)
 				}
 
 			}
@@ -592,7 +546,6 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 					index = append(index, i+1)
 					midl_sl = append(midl_sl, sr)
 					k += 1
-					fmt.Println(operation_list, used_index, index)
 				} else if sliseContains(index, i-1) == true && sliseContains(index, i) == false && sliseContains(index, i+1) == false {
 					sr := []string{}
 					fp := ""
@@ -626,12 +579,8 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 					index = append(index, i)
 					index = append(index, i+1)
 
-					// sr1 := strings.Join(sr, "")
-					// sr2 = append(sr2, sr1)
 					midl_sl = append(midl_sl, sr)
-					// operation_list[strconv.Itoa(ri)] = midl_sl
 					k += 1
-					fmt.Println(operation_list, used_index, index)
 				} else if sliseContains(index, i-1) == false && sliseContains(index, i) == false && sliseContains(index, i+1) == true {
 					sr := []string{}
 					fp := ""
@@ -665,12 +614,8 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 					index = append(index, i)
 					index = append(index, i+1)
 
-					// sr1 := strings.Join(sr, "")
-					// sr2 = append(sr2, sr1)
 					midl_sl = append(midl_sl, sr)
-					// operation_list[strconv.Itoa(ri)] = midl_sl
 					k += 1
-					fmt.Println(operation_list, used_index, index)
 				} else if sliseContains(index, i-1) == true && sliseContains(index, i) == false && sliseContains(index, i+1) == true {
 					sr := []string{}
 					fp := ""
@@ -710,7 +655,6 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 							}
 						}
 					}
-
 					sr = append(sr, fp)
 					sr = append(sr, expr2[i])
 					sr = append(sr, fp1)
@@ -723,36 +667,28 @@ func parseExpr(expr string, ri int) (map_expr map[int][][]string, err string) {
 					index = append(index, i)
 					index = append(index, i+1)
 
-					// sr1 := strings.Join(sr, "")
-					// sr2 = append(sr2, sr1)
 					midl_sl = append(midl_sl, sr)
-					// operation_list[strconv.Itoa(ri)] = midl_sl
 					k += 1
-					fmt.Println(operation_list, used_index, index)
 				}
 			}
 		}
 	}
 	operation_list[ri] = midl_sl
-	fmt.Println(operation_list)
-	// if expr != "" {
 	return operation_list, "200. Выражение успешно принято, распаршено и принято к обработке"
-	// }
-	// return operation_list, "400. Выражение невалидно"
 }
 
 func UpdateStatus(filename, key, value, id string) error {
-	// Read the file into memory.
+	// чтение файла
 	data, err := GetStatus(filename)
 	if err != nil {
 		return err
 	}
 
-	// Update the map in memory.
+	// обновление значения
 	value1 := value + " " + id
 	data[key] = value1
 
-	// Write the entire map back to the file.
+	// запись обновлённого обратно
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
@@ -796,16 +732,11 @@ func GetStatus(filename string) (map[string]string, error) {
 }
 
 func WriteToFile(filename, key, value string) error {
-	// Read the file into memory.
 	data, err := ReadFromFile(filename)
 	if err != nil {
 		return err
 	}
-
-	// Update the map in memory.
 	data[key] = value
-
-	// Write the entire map back to the file.
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
@@ -860,7 +791,6 @@ func incrementNumberInFile(filename string) (int, error) {
 	}
 	num++
 	newContent := strconv.Itoa(num)
-	// Write the new content back to the file
 	err = ioutil.WriteFile(filename, []byte(newContent), 0644)
 	if err != nil {
 		return 0, err
@@ -868,20 +798,21 @@ func incrementNumberInFile(filename string) (int, error) {
 	return num, nil
 }
 
+// основная функция работающая с выражением
 func Orchestrat(w http.ResponseWriter, r *http.Request) {
 	expr := r.URL.Query().Get("expr")
-	fmt.Println(expr)
 	var message string
 	message = "400. Выражение невалидно"
+	// проверка выражения
 	if checkArithmeticExpression(expr) == true && strings.Contains(expr, " ") == false && expr != "" {
 		data1, err := ReadFromFile("db.txt")
 		if err != nil {
 			log.Fatal(err)
 		}
+		// проверка выражения на повторение
 		repiated_expr := false
 		for e, _ := range data1 {
 			if e == expr {
-				// message = "400. Выражение невалидно"
 				repiated_expr = true
 			}
 		}
@@ -904,29 +835,11 @@ func Orchestrat(w http.ResponseWriter, r *http.Request) {
 
 			recvest_id[expr] = ri
 			len_expr[expr] = len(pars[ri])
-			fmt.Println(len_expr[expr])
 			sendToAgent(pars, expr, ri)
 			message = "200. Выражение успешно принято, распаршено и принято к обработке"
 		} else {
 			message = "200. Выражение успешно принято, распаршено и принято к обработке"
 		}
-		// message = invalid_expr
-		// if len(pars[ri]) > (agent_1 + agent_2) {
-		// 	message = "На серверах не хватает горутин, для обработки сообщения"
-		// } else {
-		// 	message = "200. Выражение успешно принято, распаршено и принято к обработке"
-
-		// 	// db[expr] = "?"
-		// 	err := WriteToFile("db.txt", expr, "?")
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-
-		// 	recvest_id[expr] = ri
-		// 	len_expr[expr] = len(pars[ri])
-		// 	fmt.Println(len_expr[expr])
-		// 	sendToAgent(pars, expr, ri)
-		// }
 	}
 	tmpl, err := template.ParseFiles("orchestrat.tmpl")
 	if err != nil {
@@ -934,56 +847,31 @@ func Orchestrat(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Pass the message directly to the template by using a map
+	// вывод сообщения
 	data := map[string]string{
 		"Message": message,
 	}
-	err = tmpl.Execute(w, data) // Execute the template with the data
+	err = tmpl.Execute(w, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
 }
 
-var fin_expr = make(map[string]int)
+// var fin_expr = make(map[string]int)
 
+// Страница с выражениями
 func Storage(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("storage.tmpl")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// data, err := ReadFromFile("db.txt")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(db)
-	// fmt.Println("storage start", data)
-	// for expr, _ := range data {
-	// 	r, err := recvest_id[expr]
-	// 	if err == true {
-	// 		q := len(db22[r])
-	// 		fmt.Println("4")
-	// 		// qq := strings.Join(q, "+")
-	// 		if q == len_expr[expr] {
-
-	// 			err := WriteToFile("db.txt", expr, db22[r][q-1][1])
-	// 			if err != nil {
-	// 				log.Fatal(err)
-	// 			}
-	// 			// data[expr] = db22[r][q-1][1]
-	// 		}
-	// 		fmt.Println("4")
-	// 	}
-
-	// }
 	data1, err := ReadFromFile("db.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println("storage", data1)
 	tmpl.Execute(w, data1)
-	// fmt.Fprint(w, db)
 }
 
 func Agents(w http.ResponseWriter, r *http.Request) {
@@ -1038,17 +926,18 @@ func OperationTime(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Функция для обновление глобальных переменных из POST-запроса
 func getFormValue(r *http.Request, key string) int {
 	value := r.FormValue(key)
 	intValue, err := strconv.Atoi(value)
 	if err != nil {
-		// Обработка ошибки преобразования строки в число
 		fmt.Println("Error parsing form value:", err)
 		return 0
 	}
 	return intValue
 }
 
+// главная страница
 func serveStaticFile(filename string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, filename)
@@ -1056,14 +945,15 @@ func serveStaticFile(filename string) http.HandlerFunc {
 }
 
 func main() {
-
+	// установка значений по умолчанию
 	additionSeconds = 5
 	subtractionSeconds = 5
 	multiplicationSeconds = 5
 	divisionSeconds = 5
 	agentConnected["8080"] = "waiting"
 	agentConnected["8081"] = "waiting"
-	// Запускаем первый агент
+
+	// Запуск первого агента
 	listener1, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal(err)
@@ -1080,7 +970,7 @@ func main() {
 		}
 	}()
 
-	// Запускаем второй агент
+	// Запуск второго агента
 	listener2, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatal(err)
@@ -1096,7 +986,7 @@ func main() {
 			go handleConnection(conn)
 		}
 	}()
-
+	// Проверка на незавершённые процессы при закрытии программы
 	status_data, err := GetStatus("status.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -1105,7 +995,6 @@ func main() {
 		status_id := strings.Split(status, " ")
 		status1 := status_id[0]
 		id, _ := strconv.Atoi(status_id[1])
-		fmt.Println(status1, id)
 		if status1 == "false" {
 			pars, _ := parseExpr(key, id)
 			recvest_id[key] = id
